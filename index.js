2ee5d0542da4e88130caff4c5340d45755e75c61
muslimumarov <norbekumarov613@gmail.com>
2023-09-25 08:48:41 +0500

    xato to'g'irlandi

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 eltitle = document.querySelector('.title');
 let elspan1 = document.querySelector('.span')
 let sayohat = +prompt('Summani kiriting');
+let USD = 12200;
+let EURO = 13500;
 let samalyot = 500;
 let mehmonxona = 250;
 let muzey = 120;
-let umumiy = 500 * 12300 + 250 * 12500 + 120 * 13500;
-console.log(umumiy);
+let umumiy = samalyot * USD + mehmonxona * USD + muzey * EURO;
 if (sayohat >= umumiy) {
 elspan1.textContent = 'oq yo\'l Alisher'
-}else {
-elspan1.textContent = 'ozgina sabr qiling Alisher aka'
-}
+}else if (sayohat <= 0)   {
+elspan1.textContent = 'tog\'ri summa kiriting!'
+} else {
+  elspan1.textContent = 'ozgina sabr qiling Alisher aka'
+  };
